a9d9d8249a011c4b0f88ad35358a63d96aac5092
Richard Stibbard <richardstibbard@gmail.com>
2020-05-06 13:15:24 +0100

    Updated type returned by methods

--- tricksy-01-shopping.cl.php
+++ tricksy-01-shopping.cl.php
@@ -2,38 +2,6 @@
 
 declare(strict_types=1);
 
-class Basket
-{
-    private $items = [];  // Set empty $items array
-
-    public function add($item)
-    {
-        $this->items[] = $item; // Add a new item into $items array 
-    }
-
-    public function total()
-    {
-        $total = 0;
-
-        foreach ($this->items as $item) {
-            $total += $item->getPrice();  // Loop, adding price from BasketItem class for each item passed in here
-        }
-
-        return "£" . number_format($total, 2);
-    }
-
-    public function items()
-    {
-        $names = [];  // Use a different name than "items"
-
-        foreach ($this->items as $item) {
-            $names[] = $item->getName();
-        }
-
-        return $names;
-    }
-}
-
 class BasketItem
 {
     private $name;
@@ -56,13 +24,45 @@ class BasketItem
     }
 }
 
+class Basket
+{
+    private $items = [];  // Set empty $items array
+
+    public function add(BasketItem $item): Basket
+    {
+        $this->items[] = $item; // Add a new item into $items array 
+        return $this;
+    }
+
+    public function total(): string
+    {
+        $total = 0;
+
+        foreach ($this->items as $item) {
+            $total += $item->getPrice();  // Loop, adding price from BasketItem class for each item passed in here
+        }
+
+        return "£" . number_format($total, 2);
+    }
+
+    public function items()
+    {
+        $names = [];  // Use a different name than "items"
+
+        foreach ($this->items as $item) {
+            $names[] = $item->getName();
+        }
+
+        return $names;
+    }
+}
+
 $basket = new Basket();
 
 $item1 = new BasketItem("coffee", 3.50);
 $item2 = new BasketItem("tea", 2.50);
 
-$basket->add($item1);
-$basket->add($item2);
+$basket->add($item1)->add($item2);
 
 var_dump($basket->total()); // string(6) £6.00
 var_dump($basket->items()); // array(2) { [0]=> string(6) "coffee" [1]=> string(3) "tea" }
