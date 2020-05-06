c183be66075a954528a57ea6e2cd6000e1a52d5f
Richard Stibbard <richardstibbard@gmail.com>
2020-05-06 12:47:45 +0100

    Removed comments

--- tricksy-01-shopping.cl.php
+++ tricksy-01-shopping.cl.php
@@ -1,9 +1,5 @@
 <?php
 
-/*Create an class that represents a shopping basket. It should have a property, items, that keeps track of the items in the basket. It should have an add method to add items to the basket. It should have a total method that returns the value of all the items' prices. It should have a items method that returns an array of item names.
-Hint: basket items will need to be an class too
-Hint: PHP has a number_format function */
-
 declare(strict_types=1);
 
 class Basket
