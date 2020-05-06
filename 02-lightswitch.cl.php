9df09a4f97824cf741d8adb0ad4308fe8446ec7b
Richard Stibbard <richardstibbard@gmail.com>
2020-05-06 08:59:33 +0100

    Lightswitch - WORKING

---
+++ 02-lightswitch.cl.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+class LightSwitch
+{
+    private $on = false;
+
+    public function turnOn(): void
+    {
+        $this->on = true;  //Nothing to return, so void
+    }
+
+    public function turnOff(): void
+    {
+        $this->on = false;
+    }
+
+    public function isOn(): bool
+    {
+        return $this->on;
+    }
+}
+
+$lightSwitch = new LightSwitch();
+
+// you can check whether it is on or not
+var_dump($lightSwitch->isOn()); // bool(false)
+
+// you can turn it on
+$lightSwitch->turnOn();
+var_dump($lightSwitch->isOn()); // bool(true)
+
+// you can turn it off
+$lightSwitch->turnOff();
+var_dump($lightSwitch->isOn()); // bool(false)
